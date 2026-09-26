669233918c3393b0d2c9907486706ef7a2ba5b77
SkyDreamsID <putrarifkinol7@gmail.com>
2026-09-26 19:34:46 +0700

    update pertemuan 4 (ubah variabel biar gamoang dibaca)

--- [Semester 1] Pemrograman Terstruktur/[2026-09-23][Pertemuan 4] Looping/resistorparalel.c
+++ [Semester 1] Pemrograman Terstruktur/[2026-09-23][Pertemuan 4] Looping/resistorparalel.c
@@ -3,6 +3,7 @@
 int jumlah_resistor;
 int i;
 float nilai_resistor;
+float satu_per_r_total;
 float r_total;
 
 void main(){
@@ -12,11 +13,10 @@ void main(){
         printf("Masukkan nilai Resistor %d : ", i);
         scanf("%f", &nilai_resistor);
 
-        r_total += (1.0 / nilai_resistor);
+        satu_per_r_total += (1.0 / nilai_resistor);
     }
 
-    r_total = 1.0 / r_total;
+    r_total = 1.0 / satu_per_r_total;
     
-
     printf("\nTotal Resistor Paralel adalah : %.2f Ohm\n", r_total);
 }
